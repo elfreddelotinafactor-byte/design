1248936f34df6042d4fde419c46a029ce2556095
elfreddelotinafactor-byte <takidelotina@gmail.com>
2026-04-22 02:56:32 +0800

    security: remove firebase-config.js from repo, add example template and gitignore

---
+++ firebase-config.example.js
@@ -0,0 +1,14 @@
+// Rename this file to firebase-config.js and fill in your values.
+
+window.__FIREBASE_CONFIG = {
+  apiKey:            "REPLACE_ME",
+  authDomain:        "REPLACE_ME.firebaseapp.com",
+  projectId:         "REPLACE_ME",
+  storageBucket:     "REPLACE_ME.firebasestorage.app",
+  messagingSenderId: "REPLACE_ME",
+  appId:             "REPLACE_ME"
+};
+
+window.__SHEETS_URL = "REPLACE_ME";
+
+window.__ADMIN_PASSPHRASE = "REPLACE_ME";
